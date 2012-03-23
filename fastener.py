a2afdac0207662471b56500314fd66dbca48894d
Erik Evenson <erik.e.evenson@gmail.com>
2012-03-22 20:23:22 -0500

    Adds some basic FreeCAD code to create a (poor) representation of a fastener.

--- fastener.py
+++ fastener.py
@@ -10,17 +10,47 @@ Copyright (c) 2012 3E Enterprises, LLC. All rights reserved.
 import sys
 import os
 
+FREECADPATH = '/usr/lib/freecad/lib'
+SAVEPATH = '/mnt/hgfs/eevenson/Documents/Projects/FreeCADScripting'
+sys.path.append(FREECADPATH)
+
 class Fastener():
 	
 	def representation(self):
-		return None
+		
+		"""
+		Returns a CAD representation of the fastener.  Defaults to a FreeCAD object.
+		"""
+		
+		import FreeCAD
+		import Part
+
+		name = "fastener"
+		size = 10
+		part = FreeCAD.ActiveDocument.addObject("Part::Feature",name)
+		
+		# Just create a simple object for now.
+		fastener = Part.makeBox(size, size, size)
+		part.Shape = fastener
+
+		return part
 		
 	def strength(self):
-		return None
+		
+		"""
+		Returns a dictionary of strength parameters.
+		"""
+		
+		return {}
 
 
 def main():
-	pass
+	
+	"""
+	Generates a sample default fastener.
+	"""
+	fastener = Fastener()
+	print fastener
 
 
 if __name__ == '__main__':
