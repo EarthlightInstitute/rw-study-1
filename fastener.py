b2da96d0f17d9603f28b491f371899ced8109f24
Erik Evenson <erik.e.evenson@gmail.com>
2012-03-22 19:44:07 -0500

    Adds stub of fastener class.

---
+++ fastener.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+fastener.py
+
+Created by Erik Evenson on 2012-03-22.
+Copyright (c) 2012 3E Enterprises, LLC. All rights reserved.
+"""
+
+import sys
+import os
+
+class Fastener():
+	
+	def representation(self):
+		return None
+		
+	def strength(self):
+		return None
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
